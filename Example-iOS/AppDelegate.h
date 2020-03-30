5fe371d19de8421710240039cca4283d746b600a
KrishnDip <nilapps@gmail.com>
2020-03-30 16:46:08 +0530

    Initial Commit

---
+++ Example-iOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Example-iOS
+//
+//  Created by kishikawa katsumi on 2015/01/15.
+//  Copyright (c) 2015 kishikawa katsumi. All rights reserved.
+//
+
+//https://github.com/kishikawakatsumi/UICKeyChainStore
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
